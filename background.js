59bf85b4fbfb68c4d08a4791138060377b454866
Kaushik Sarma <kaushik.sarma@flipkart.com>
2020-09-13 12:04:39 +0530

    adding rating to UI

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 (async () => {
 	window.onload = () => {
 		// create dom mutation observer
-		const config = { attributes: false, childList: true, subtree: true };
+		const config = { attributes: false, childList: true, subtree: false };
 		const targetElement = document.getElementsByClassName('netflix-sans-font-loaded')[0].firstElementChild.firstElementChild;
 		var observer = new MutationObserver((mutationList) => {
 			mutationList.forEach(mutation => {
@@ -12,7 +12,21 @@
 						fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}&r=json`)
 						.then(response => response.json())
 						.then(r => {
-							console.log(`${title} rated ${r.imdbRating}`);
+							if(r.imdbRating) {
+								const content = document.createElement('div');
+								content.innerHTML = `Rating: <span style="color:#ffffff;text-shadow: 1px 1px 5px #E50914;">`
+													+`${r.imdbRating}</span> / 10</div>`;
+;
+								// miniModalList[0].getElementsByClassName('previewModal--boxart')[0].innerHTML 
+								// += `<div style="display:block;font-weight:900">Rating: <span style="color:#ffffff;text-shadow: 1px 1px 5px #E50914;">`
+								// +`${r.imdbRating}</span> / 10</div>`;
+								const container =  miniModalList[0].getElementsByClassName('previewModal--metadatAndControls-container')[0];
+								if(container.nextSibling) {
+									container.parentNode.insertBefore(content, container.nextSibling);
+								} else {
+									container.parentNode.appendChild(content);
+								}
+							}
 						});
 					}
 				}
